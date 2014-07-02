efc8cc04e53e71aec27507e9685035a21be5bbd7
Pronin Alexander <pronin.alx@gmail.com>
2014-07-02 16:15:32 +0300

    Initial Commit

---
+++ UpSliderMenu/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  UpSliderMenu
+//
+//  Created by Aleksandr Pronin on 02.07.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
