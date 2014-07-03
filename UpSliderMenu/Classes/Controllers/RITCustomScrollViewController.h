b79151b143ff948219ebae77865d884e51f85706
Pronin Alexander <pronin.alx@gmail.com>
2014-07-03 18:36:11 +0300

    Scrolling and Zooming a View Hierarchy

---
+++ UpSliderMenu/Classes/Controllers/RITCustomScrollViewController.h
@@ -0,0 +1,15 @@
+//
+//  RITCustomScrollViewController.h
+//  UpSliderMenu
+//
+//  Created by Aleksandr Pronin on 03.07.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITCustomScrollViewController : UIViewController <UIScrollViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+
+@end
